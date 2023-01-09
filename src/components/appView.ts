94b2c51cdff1309d4331fc1756050fdb11f92a4e
Alexander Megadrive <alexmihailov91@gmail.com>
2023-01-09 21:25:53 +0300

    fat: add appview class

---
+++ src/components/appView.ts
@@ -0,0 +1,89 @@
+import { getQueryParams } from '../utils/getQueryParams';
+
+//class for common render functions
+class AppView {
+  updateCartItemsTotal(id: number, itemCountInCart: number, totalSum: number, totalCount: number) {
+    const cartBtnContaier = document.querySelector(`[data-cartBtn-id="${id}"]`);
+    const totalSumHeader = document.querySelector('#header-total-sum');
+    const totalCountHeader = document.querySelector('#header-total-count');
+    const totalSumCart = document.querySelector('#cart-total-sum');
+    const totalProductCart = document.querySelector('#cart-total-product');
+
+    if (totalSumHeader && totalCountHeader) {
+      totalSumHeader.innerHTML = totalSum.toString();
+      totalCountHeader.innerHTML = totalCount.toString();
+    }
+    if (totalSumCart && totalProductCart) {
+      totalSumCart.innerHTML = totalSum.toString();
+      totalProductCart.innerHTML = totalCount.toString();
+    }
+    if (itemCountInCart == -1) {
+      const cartItemCard = document.querySelector(`[data-productcard-id="${id}"]`);
+      cartItemCard?.remove();
+      return;
+    }
+
+    if (cartBtnContaier) {
+      cartBtnContaier.innerHTML = this.renderCartBtnContainer(id, itemCountInCart);
+    }
+  }
+  renderCartBtnContainer(id: number, itemCountInCart: number) {
+    const newItemBtn = `
+          <button class="item__cart-addBtn" data-cart-action="new" data-product-id="${id}">Add to cart</button>`;
+    const existingItemBtns = `
+          <div class="item__cart-countControlBtns">
+          <button class="item__buy-btn" data-cart-action="decrease" data-product-id="${id}">-</button>
+          <span class="item__buy-number">${itemCountInCart}</span>
+          <button class="item__buy-btn" data-cart-action="increase" data-product-id="${id}">+</button>
+          </div>
+        `;
+    return itemCountInCart > 0 ? existingItemBtns : newItemBtn;
+  }
+
+  setURL(key: string, value: string | null) {
+    if (!value) return;
+    else value = value.toLowerCase();
+    const url = document.location.href;
+    const queryParams = getQueryParams(url);
+
+    let path = url.split('?')[0];
+
+    if (key in queryParams && (key == 'color' || key == 'brand' || key == 'category')) {
+      // if key is color or brand or category
+      if (!queryParams[key].split(',').includes(value)) {
+        // and  if there is no such value, add it
+        const keyArr = queryParams[key].split(',');
+        keyArr.push(value);
+        const keyStr = keyArr.join('%2C');
+        queryParams[key] = keyStr;
+      } else {
+        // remove value if already has such value
+        const valueArr = queryParams[key].split(',');
+        const index = valueArr.findIndex((el) => el == value);
+        valueArr.splice(index, 1);
+        const valueStr = valueArr.join('%2C');
+        queryParams[key] = valueStr;
+      }
+    } else {
+      // not category/brand/color,  rewrite exising key
+      queryParams[key] = value.toString().split(' ').join('%20');
+    }
+
+    // constructing query string
+    let queryString = '';
+    const queryArr: Array<string> = [];
+    for (const key in queryParams) {
+      if (Object.prototype.hasOwnProperty.call(queryParams, key)) {
+        if (queryParams[key].length > 0) {
+          const element = `${key}=${queryParams[key]}`;
+          if (element) queryArr.push(element);
+        } else delete queryParams[key];
+      }
+    }
+    path = path + (Object.keys(queryParams).length > 0 ? '?' : '');
+    queryString = queryArr.length > 0 ? queryArr.join('&') : '';
+    window.history.pushState('Catalog', 'Catalog | RC Toys', path + queryString);
+  }
+}
+
+export default AppView;
